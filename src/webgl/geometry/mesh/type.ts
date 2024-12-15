45e77e7b26507db5b4739b4c8c01e5fb0c5bbbd1
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-15 22:40:27 +0100

    feat: maxDistance on Mesh type

--- src/webgl/geometry/mesh/type.ts
+++ src/webgl/geometry/mesh/type.ts
@@ -1,7 +1,17 @@
-import { V3 } from '../helpers/v3';
+import { V3 } from '../helpers/v3'
 
+/**
+ * A basic mesh
+ * @type {Mesh}
+ * vertices: vertices of the mesh
+ * normals: normals of the vertices
+ * faces: faces of the mesh, an array of vertex indexes (should be 3 or more)
+ * faceType: (optional) type of the face
+ */
 export type Mesh = {
-  vertices: V3[];
-  faces: number[][];
-  normals: V3[];
-};
+  vertices: V3[]
+  faces: number[][]
+  normals: V3[]
+  maxDistances?: number[]
+  faceType?: number[]
+}
