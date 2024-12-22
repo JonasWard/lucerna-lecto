16c39ac08bdc780ce6437e9109c7b49a22d8ea84
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-22 18:58:54 +0100

    feat: benchmarking wrapper method

--- src/webgl/helpermethods.ts
+++ src/webgl/helpermethods.ts
@@ -25,3 +25,18 @@ export const getVertexHash = (v: V3): string =>
   `${v.x.toFixed(2).replace('-0.00', '0.00')}_${v.y.toFixed(2).replace('-0.00', '0.00')}_${v.z
     .toFixed(2)
     .replace('-0.00', '0.00')}`
+
+/**
+ * Benchmarking helper method that wraps a method, returns it output and measure the output
+ * @param fn - () => any method to benchmark
+ * @param text - optional name / decsription of the method (displays as <text> took: <time> ms), default is the method name
+ * @returns output of the method
+ */
+export const measurePerformance = (fn: () => any, text?: string) => {
+  const start = performance.now()
+  const output = fn()
+  const end = performance.now()
+  const fText = text ?? fn.name
+  console.log(`${fText} took: ${(end - start).toFixed(2)} ms`)
+  return output
+}
