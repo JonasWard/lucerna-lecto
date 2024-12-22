7413df804abce38b1db4982165a6b4cf112c4d68
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-12-22 18:59:17 +0100

    feat: improved performance of vertexData method

--- src/webgl/geometry/mesh/vertexData.ts
+++ src/webgl/geometry/mesh/vertexData.ts
@@ -1,31 +1,35 @@
-import { V3 } from '../helpers/v3';
-import { VertexData } from '../helpers/vertexData';
-import { Mesh } from './type';
+import { V3 } from '../helpers/v3'
+import { VertexData } from '../helpers/vertexData'
+import { Mesh } from './type'
 
 const triangulateIndices = (indices: number[], indicesArray: number[] = []) => {
-  if (indices.length === 3) return indicesArray.push(...indices);
-  else if (indices.length > 3) for (let i = 0; i < indices.length - 2; i++) triangulateIndices([indices[0], indices[i + 1], indices[i + 2]], indicesArray);
-};
+  if (indices.length === 3) return indicesArray.push(...indices)
+  else if (indices.length > 3)
+    for (let i = 0; i < indices.length - 2; i++)
+      triangulateIndices([indices[0], indices[i + 1], indices[i + 2]], indicesArray)
+}
 
-const mapV3 = (v3: V3, vArray: number[]) => vArray.push(v3.x, v3.z, -v3.y);
+const mapV3 = (v3: V3, vArray: number[]) => vArray.push(v3.x, v3.z, -v3.y)
 
 export const getVertexDataForMesh = (mesh: Mesh): VertexData => {
-  const indices: number[] = [];
+  const indices: number[] = []
 
-  mesh.faces.forEach((face) => triangulateIndices(face, indices));
+  for (let i = 0; i < mesh.faces.length; i++) triangulateIndices(mesh.faces[i], indices) // for (let i = 0; i < mesh.faces.length; i++) triangulateIndices(face, indices))
 
-  const positions: number[] = [];
-  const normals: number[] = [];
+  const positions: number[] = []
+  const normals: number[] = []
 
-  mesh.vertices.map((v) => mapV3(v, positions));
-  mesh.normals.map((n) => mapV3(n, normals));
+  for (let i = 0; i < mesh.vertices.length; i++) {
+    mapV3(mesh.vertices[i], positions)
+    mapV3(mesh.normals[i], normals)
+  }
 
   const vertexData: VertexData = {
     positions: new Float32Array(positions),
     indices,
     normals: new Float32Array(normals),
     uvs: new Float32Array([]),
-  };
+  }
 
-  return vertexData;
-};
+  return vertexData
+}
