ea275f6118ae2d61e5eb30a2793e4bacb0c19e5d
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-05-05 15:21:03 +0200

    feat: fixed styling

--- src/Components/parametrics/ParametricInput.tsx
+++ src/Components/parametrics/ParametricInput.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { DisplayType, StateDataRenderer } from './StateDataRenderer'
 import { AttributeNames } from '../../modelDefinition/enums/attributeNames'
-import { shouldUseDrawer } from '../utils/windowMethods'
-import { EnumSemantics } from 'url-safe-bitpacking/dist/types'
+import { DataEntry, EnumSemantics, StateDataType } from 'url-safe-bitpacking/dist/types'
 import { useData } from '../../state/state'
+import { Version0Type } from 'src/modelDefinition/types/version0.generatedType'
+import { IconRenderer } from './IconRenderer'
+import { GrClose } from 'react-icons/gr'
+import { DataEntryRenderer } from './dataentryrenderers/DataEntryRenderer'
 
 const displayTypeMap = {
   [AttributeNames.Viewport]: DisplayType.NESTED,
@@ -18,27 +21,63 @@ const displayTypeMap = {
   [AttributeNames.GlobalGeometry]: DisplayType.NESTED,
 }
 
+const mainKeys: (keyof Version0Type)[] = [
+  ...(import.meta.env.DEV ? ([AttributeNames.Version, AttributeNames.Viewport] as (keyof Version0Type)[]) : []),
+  AttributeNames.LampShades,
+  AttributeNames.Pattern,
+  AttributeNames.Material,
+  AttributeNames.VerticalProfile,
+  AttributeNames.Visualization,
+  AttributeNames.GlobalGeometry,
+]
+
 type IParametricInputProps = {
   versionEnumSemantics?: EnumSemantics
 }
 
 export const ParametricInput: React.FC<IParametricInputProps> = ({ versionEnumSemantics }) => {
   const data = useData((s) => s.data)
-  const updateEntry = useData((s) => s.updateDataEntry)
-  const [activeName, setActiveName] = React.useState('')
+  const activeName = useData((s) => s.activeName)
+  const setActiveName = useData((s) => s.setActiveName)
+  const clearActiveName = useData((s) => s.clearActiveName)
 
   return (
-    <div style={{ position: 'fixed', left: 10, top: 10, padding: 8 }}>
-      <StateDataRenderer
-        asSlider
-        data={data}
-        name={''} // name is not used in this context
-        updateEntry={updateEntry}
-        versionEnumSemantics={versionEnumSemantics}
-        activeName={activeName}
-        setActiveName={setActiveName}
-        displayTypeMap={displayTypeMap}
-      />
+    <div className={`parametric-input-top-level ${activeName ? 'active' : ''}`}>
+      <div className="parametric-input-icons">
+        {mainKeys.map((k) => (
+          <div className={`icon-button ${k === activeName ? 'active' : ''}`} key={k} onClick={() => setActiveName(k)}>
+            <IconRenderer name={k} noName />
+          </div>
+        ))}
+      </div>
+      <div className={`parametric-input-content ${activeName ? 'active' : ''}`}>
+        {activeName ? (
+          <span className="parametric-input-title">
+            <IconRenderer name={activeName} />
+            <GrClose onClick={clearActiveName} />
+          </span>
+        ) : (
+          <span></span>
+        )}
+        <div className={`parametric-input-fields`}>
+          {mainKeys.map((k) =>
+            activeName === k ? (
+              k === AttributeNames.Version ? (
+                <DataEntryRenderer key={k} dataEntry={data[k] as DataEntry} />
+              ) : (
+                <StateDataRenderer
+                  key={k}
+                  asSlider
+                  data={data[k] as StateDataType}
+                  name={k}
+                  versionEnumSemantics={versionEnumSemantics}
+                  displayTypeMap={displayTypeMap}
+                />
+              )
+            ) : null
+          )}
+        </div>
+      </div>
     </div>
   )
 }
